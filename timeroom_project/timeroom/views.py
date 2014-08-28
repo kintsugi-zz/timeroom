b8b1090830c44628823bacd4e2f8276cc65a3803
kintsugi <kintsugi@users.noreply.github.com>
2014-08-28 16:28:07 -0500

    Initial commit #2 - views/url templates added

--- timeroom_project/timeroom/views.py
+++ timeroom_project/timeroom/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, render_to_response, redirect
+from django.template import RequestContext
 
 # Create your views here.
+def index(request):
+	#request = RequestContext(request)
+	return HttpResponse("the beginning of time")
